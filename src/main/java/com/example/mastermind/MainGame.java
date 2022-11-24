2d4ad6ee90b9d52f2f2461228f12b20d3b725f13
nickkrassas <nikrass@cs.ihu.gr>
2022-11-25 01:43:15 +0200

    mastermind game

--- src/main/java/com/example/mastermind/MainGame.java
+++ src/main/java/com/example/mastermind/MainGame.java
@@ -11,6 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,19 +23,31 @@ public class MainGame {
     @FXML
     public Circle circle11;
     @FXML
-    private Rectangle white,blue,black,yellow,green,orange,purple,red;
+    public Rectangle white,blue,black,yellow,green,orange,purple,red;
 
+    public Paint color;
     public void colorGetter (MouseEvent event){
-        
+
+        String pick = new String();
+        pick = event.getPickResult().getIntersectedNode().getId();
+        switch (pick) {
+            case "white" -> color = white.getFill();
+            case "blue" -> color = blue.getFill();
+            case "black" -> color = black.getFill();
+            case "yellow" -> color = yellow.getFill();
+            case "green" -> color = green.getFill();
+            case "orange" -> color = orange.getFill();
+            case "purple" -> color = purple.getFill();
+            case "red" -> color = red.getFill();
+        }
     }
 
     public void colorPicker (MouseEvent event){
-        String colorPicked = new String();
         String eventlogger = event.getPickResult().getIntersectedNode().getId();
         String selectedcircleId = eventlogger.replaceAll("[\\D]", "");
         int idx = Integer.parseInt(selectedcircleId);
         System.out.print(idx);
-        circle11.setFill(Paint.valueOf(colorPicked));
+        circle11.setFill(color);
     }
 
 }
