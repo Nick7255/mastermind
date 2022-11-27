6d81143682a6c32deda70f9334c9eb65b8c15927
Kostas Markopoulos <kostasmark151@gmail.com>
2022-11-27 16:48:06 +0200

    flags

--- src/main/java/com/example/mastermind/MainGame.java
+++ src/main/java/com/example/mastermind/MainGame.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -20,6 +21,12 @@ public class MainGame {
             circle17, circle18, circle19, circle20, circle21, circle22, circle23, circle24, circle25,
             circle26, circle27, circle28, circle29, circle30, circle31, circle32, circle33,
             circle34, circle35, circle36, circle37, circle38, circle39, circle40, hiddenCircle1, hiddenCircle2, hiddenCircle3, hiddenCircle4;
+
+    @FXML
+    public Circle flag1,flag2,flag3,flag4,flag5,flag6,flag7,flag8,flag9,flag10,
+            flag11,flag12,flag13,flag14,flag15,flag16,flag17,flag18,flag19,flag20,
+            flag21,flag22,flag23,flag24,flag25,flag26,flag27,flag28,flag29,flag30,
+            flag31,flag32,flag33,flag34,flag35,flag36,flag37,flag38,flag39,flag40;
 
 
     @FXML
@@ -58,6 +65,7 @@ public class MainGame {
             circle.setFill(color);
             circle.setDisable(true);
             circles.add(circle);
+
         }
         if (circles.size() == 4) {
             System.out.println("4 circles complete");
@@ -83,18 +91,56 @@ public class MainGame {
             colorsarray.add(rectangle.getFill());
         }
 
-        for (Circle circle41 : Arrays.asList(hiddenCircle1, hiddenCircle2, hiddenCircle3, hiddenCircle4)) {
+        for (Circle circleHidden : Arrays.asList(hiddenCircle1, hiddenCircle2, hiddenCircle3, hiddenCircle4)) {
             String i = String.valueOf(randNum());
             System.out.println(i);
-           circle41.setFill(colorsarray.get(randNum()));
+           circleHidden.setFill(colorsarray.get(randNum()));
+           circleHidden.setDisable(true);
+           circleHidden.setVisible(false);
         }
     }
 
+    public Circle[][] hint= new Circle[][]{
+            {flag1,flag2,flag3,flag4},
+            {flag5,flag6,flag7,flag8},
+            {flag9,flag10,flag11,flag12},
+            {flag13,flag14,flag15,flag16},
+            {flag17,flag18,flag19,flag20},
+            {flag21,flag22,flag23,flag24},
+            {flag25,flag26,flag27,flag28},
+            {flag29,flag30,flag31,flag32},
+            {flag33,flag34,flag35,flag36},
+            {flag37,flag38,flag39,flag40},
+    };
+
+    int round= 0;
+    int id=0;
+
+    private void flagsSetUp(int colorExist,int idx){
+        if (colorExist == 0){
+            hint[round][idx].setFill(Color.WHITE);
+        }
+        if (colorExist == 1){
+            hint[round][idx].setFill(Color.BLACK);
+        }
+    }
+
+
+    //Gia kathe swsto xrwma deixnei white flag & Gia kathe xrwma poy exei mantepsei swsta thn thesi deixnei black flag
+
     public void Submit (ActionEvent event) {
 
-
-
-
+        for (int idx=0;idx>=circles.size();idx++){
+            for (int idy=0; idy>=circles.size();idy++){
+                if(circles.get(idx) ==circleHidden.get(idy) && idx==idy){
+                    id=idy;
+                    hint[1][id];
+                } else if (circles.get(idx)==circleHidden.get(idy) && idx!=idy) {
+                    id=idy;
+                    hint[0][id];
+                }
+            }
+        }
 
         circles.clear();
         System.out.println(circles);
